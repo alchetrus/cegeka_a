69471556f79ac8fed9bf6e73c535bdf6f4f72821
alchetrus <mindrescumihai99@gmail.comm>
2023-02-23 16:58:42 +0200

    second homework

---
+++ Tema 02 - SQL & ORM/PetShelter/PetShelter.DataAccessLayer/PetShelterContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShelter.DataAccessLayer.Configuration;
+using PetShelter.DataAccessLayer.Models;
+
+namespace PetShelter.DataAccessLayer;
+
+public class PetShelterContext : DbContext
+{
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Donation> Donations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=PetShelter;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PetConfiguration());
+        modelBuilder.ApplyConfiguration(new PersonConfiguration());
+        modelBuilder.ApplyConfiguration(new DonationConfiguration());
+    }
+}
